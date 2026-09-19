62af4f1a27c8e309a38570d39e3e14cabb4f9a2a
Ni7i <187387582+Ni7i@users.noreply.github.com>
2026-09-20 00:12:01 +0200

    expand linked list implementation

--- src/data_structures/linked_list.py
+++ src/data_structures/linked_list.py
@@ -45,3 +45,40 @@ class LinkedList:
             nodes.append(str(current.data))
             current = current.next
         return " -> ".join(nodes)
+
+def delete(self, data):
+    """Delete first occurrence of data."""
+    if not self.head:
+        return False
+    if self.head.data == data:
+        self.head = self.head.next
+        self._size -= 1
+        return True
+    current = self.head
+    while current.next:
+        if current.next.data == data:
+            current.next = current.next.next
+            self._size -= 1
+            return True
+        current = current.next
+    return False
+
+def find(self, data) -> bool:
+    """Check if data exists in the list."""
+    current = self.head
+    while current:
+        if current.data == data:
+            return True
+        current = current.next
+    return False
+
+def reverse(self):
+    """Reverse the linked list in place."""
+    prev = None
+    current = self.head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    self.head = prev
